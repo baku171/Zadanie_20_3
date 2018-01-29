bf41cdf8bc8ad0408b82b082bde33c62ad0c1af4
baku171 <pbak@esri.pl>
2018-01-29 12:53:28 +0100

    delete country

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 import store from './store/index';
-import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+// import DevTools from './DevTools';
+// import { getCountries } from './actions/actions-countries';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizacja aplikacji</h1>
-            <DevTools />
-        </div>
+        <Router history={hashHistory} routes={routes}>
+        </Router>
     </Provider>, 
     document.getElementById('root')
 );
 
-store.dispatch(getCountries());
+// store.dispatch(getCountries());
